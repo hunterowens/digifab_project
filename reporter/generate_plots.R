f129115ab15f452cef703abb9354612ee9562118
Hunter Owens <owens.hunter@gmail.com>
2013-12-02 14:42:12 -0600

    gen plots fixes

--- reporter/generate_plots.R
+++ reporter/generate_plots.R
@@ -1,8 +1,9 @@
 library("ggplot2")
 
+max = 100
 
 train_graph <- read.csv('./training_data.csv',header=T)
-qplot(x,y,data=train_graph)
+qplot(x,y,data=train_graph,xmin = -max,xmax= max)
 
 predictions <- read.csv('./predictions.csv',header=T)
-qplot(x_predictions,y_predcitions,data=predictions)
+qplot(x_predictions,y_predcitions,data=predictions, xmin = -max,xmax= max)
